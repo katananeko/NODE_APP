ad5e5dfb82e2bc6c2c0a3478e1ac832d681ef10e
I-am-Naka <shohei.nakyama7777@gmail.com>
2023-11-04 17:59:43 +0900

    fix: query追加

--- app.js
+++ app.js
@@ -4,40 +4,30 @@ const ejs = require('ejs');
 const url = require('url');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
-const other_page = fs.readFileSync('./other.ejs', 'utf8');
-const style_css = fs.readFileSync('./style.css', 'utf8');
 
 var server = http.createServer(getFromClient);
 
 server.listen(3000);
 console.log('Server start!');
 
+// createServerの処理
 function getFromClient(req, res) {
-    var url_parts = url.parse(req.url);
+
+    var url_parts = url.parse(req.url, true);
     switch (url_parts.pathname){
+
         case '/':
+            var content = "これはIndexページです。"
+            var query = url_parts.query;
+            if (query.msg != undefined){
+                content += 'あなたは、「' + query.msg + '」と送りました。';
+            }
             var content = ejs.render(index_page, {
-                title: "Indexページ",
-                content: "これはIndexページです。",
+                title: "Index",
+                content: content,
             });
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(content);
-            res.end();
-            break;
-
-        case '/other':
-            var content = ejs.render(other_page, {
-                title: "Indexページ",
-                content: "これは新しく用意したページです。",
-            });
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(content);
-            res.end();
-            break;
-
-        case '/style.css':
-            res.writeHead(200, {'Content-Type': 'text/css'});
-            res.write(style_css);
             res.end();
             break;
 
