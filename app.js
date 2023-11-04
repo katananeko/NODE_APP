09a0ee412a7036ad074ac91fcb68a4a7c8017190
I-am-Naka <shohei.nakyama7777@gmail.com>
2023-11-04 17:35:10 +0900

    add: otherページ追加

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ejs = require('ejs');
 const url = require('url');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
+const other_page = fs.readFileSync('./other.ejs', 'utf8');
 const style_css = fs.readFileSync('./style.css', 'utf8');
 
 var server = http.createServer(getFromClient);
@@ -17,23 +18,32 @@ function getFromClient(req, res) {
         case '/':
             var content = ejs.render(index_page, {
                 title: "Indexページ",
-                content: "これはテンプレートを使ったサンプルページです。",
+                content: "これはIndexページです。",
             });
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(content);
             res.end();
             break;
-        
+
+        case '/other':
+            var content = ejs.render(other_page, {
+                title: "Indexページ",
+                content: "これは新しく用意したページです。",
+            });
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(content);
+            res.end();
+            break;
+
         case '/style.css':
             res.writeHead(200, {'Content-Type': 'text/css'});
             res.write(style_css);
             res.end();
             break;
-        
+
         default:
             res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end('no page...');
             break;
     }
-    
 }
