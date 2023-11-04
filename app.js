75617e26d77494de53f2cf50851d249808c2b578
I-am-Naka <shohei.nakyama7777@gmail.com>
2023-11-04 17:24:36 +0900

    add: css追加

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
+const url = require('url');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
+const style_css = fs.readFileSync('./style.css', 'utf8');
 
 var server = http.createServer(getFromClient);
 
@@ -10,11 +12,28 @@ server.listen(3000);
 console.log('Server start!');
 
 function getFromClient(req, res) {
-    var content = ejs.render(index_page, {
-        title: "Indexページ",
-        content: "これはテンプレートを使ったサンプルページです。",
-    });
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(content);
-    res.end();
+    var url_parts = url.parse(req.url);
+    switch (url_parts.pathname){
+        case '/':
+            var content = ejs.render(index_page, {
+                title: "Indexページ",
+                content: "これはテンプレートを使ったサンプルページです。",
+            });
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(content);
+            res.end();
+            break;
+        
+        case '/style.css':
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            res.write(style_css);
+            res.end();
+            break;
+        
+        default:
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('no page...');
+            break;
+    }
+    
 }
