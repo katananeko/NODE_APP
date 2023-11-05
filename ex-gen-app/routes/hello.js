9c1626d5a8778e57416d9517726911dbe695746c
I-am-Naka <shohei.nakyama7777@gmail.com>
2023-11-05 16:31:12 +0900

    fix: post機能追加

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -4,7 +4,16 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   var data = {
     title: 'Hello',
-    content: 'これは、サンプルのコンテンツです。<br>this is sample content.'
+    content: '※何か書いて送信して下さい。'
+  };
+  res.render('hello', data);
+});
+
+router.post('/post', (req, res, next) => {
+  var msg = req.body['message'];
+  var data = {
+    title: 'Hello',
+    content: 'あなたは、「' + msg + '」と送信しました。'
   };
   res.render('hello', data);
 });
